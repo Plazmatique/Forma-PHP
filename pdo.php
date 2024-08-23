e4bb656109b1ec0f8d354daae81368f40742c54c
Lucas Vanderzielen <la229879@student.helha.be>
2024-08-23 11:10:45 +0200

    Error handling

--- pdo.php
+++ pdo.php
@@ -3,8 +3,12 @@ require_once'config.php';
 $sql = 'SELECT * FROM contacts';
 $contactRq = $connect->prepare($sql);
 var_dump($contactRq);
-$contactRq->execute();
+$contactRq->execute() or die($connect->errorInfo()); //tue le script si la requête est mauvaise
+
+echo $nbContacts = $contactRq->rowCount();
+
 $contacts = $contactRq->fetchAll(PDO::FETCH_ASSOC);
+
 echo '<pre>';
 var_dump($contacts);
 echo'</pre>';
