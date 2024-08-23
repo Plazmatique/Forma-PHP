1196d280b8bcf79fdbac16f069fd4e36c0e7b06f
Lucas Vanderzielen <la229879@student.helha.be>
2024-08-23 11:06:28 +0200

    Request SQL

--- pdo.php
+++ pdo.php
@@ -1,18 +1,10 @@
 <?php
-const DB_USER = "root";
-const DB_PASS = "";
-const DB_NAME = "skills_24";
-const DB_HOST = "localhost";
-const DB_PORT = 3306;
-
-try{
-  $connect = new PDO(
-    sprintf('mysql:host=%s;dbname=%s;port=%d;charset=utf8', 
-          DB_HOST, DB_NAME, DB_PORT), 
-          DB_USER,
-          DB_PASS
-  );
-}catch(Exception $e){
-  echo $e->getMessage();
-  die();
-}
+require_once'config.php';
+$sql = 'SELECT * FROM contacts';
+$contactRq = $connect->prepare($sql);
+var_dump($contactRq);
+$contactRq->execute();
+$contacts = $contactRq->fetchAll(PDO::FETCH_ASSOC);
+echo '<pre>';
+var_dump($contacts);
+echo'</pre>';
